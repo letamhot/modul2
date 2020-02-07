315f17592632f6169ae1698ce314b2e977eb2474
LÊ VĂN TÁM <31695567+letamhot@users.noreply.github.com>
2020-02-07 11:38:53 +0700

    Update count_obj.php

--- arr/count_obj/count_obj.php
+++ arr/count_obj/count_obj.php
@@ -7,13 +7,66 @@
     <title>Count</title>
 </head>
 <body>
+<!-- <form action="count_obj.php" method="post">
+<input type="text" class="text" value="Nhập">
+<input type="submit" class="sub" value="Tổng">
+</form> -->
     <?php 
         $obj= "Hello World";
-        $count = 0;
-        echo 'Tổng số kí tự: ' . strlen($obj);
-        foreach($obj as $key => $value){
-            
-        } 
+        print_r($obj);
+        echo '<br>';
+        $count = null;
+        $count1 = null;
+        $count2 = null;
+        $count3 = null;
+        $count4 = null;
+        $count5 = null;
+        $position = null;
+        $position1 = null;
+        $position2 = null;
+        $position3 = null;
+        $position4 = null;
+        $position5 = null;
+
+        echo 'Tổng số kí tự: ' . strlen($obj) . '<br>';
+        for($i = 0; $i < strlen($obj); $i++){
+            if($obj[$i] == strtoupper('h') || $obj[$i] == strtolower('H')){
+                $position .= $i . '<br>';
+                $count++;
+            }
+            if($obj[$i] == strtoupper('e') || $obj[$i] == strtolower('E')){
+                $position1 .= $i . '<br>';
+                $count1++;
+            }
+            if($obj[$i] == strtoupper('l') || $obj[$i] == strtolower('L')){
+                $position2 .= $i . '<br>';
+                $count2++;
+            }
+            if($obj[$i] == strtoupper('o') || $obj[$i] == strtolower('O')){
+                $position3 .= $i . '<br>';
+                $count3++;
+            }
+            if($obj[$i] == strtoupper('d') || $obj[$i] == strtolower('D')){
+                $position4 .= $i . '<br>';
+                $count4++;
+            }
+            if($obj[$i] == strtoupper('w') || $obj[$i] == strtolower('W')){
+                $position5 .= $i . '<br>';
+                $count5++;
+            }
+        }
+        echo '<br>';
+        echo 'Số ký tự h: ' . $count;
+        echo '<br>';
+        echo 'Số ký tự e: ' . $count1;
+        echo '<br>';
+        echo 'Số ký tự l: ' . $count2;
+        echo '<br>';
+        echo 'Số ký tự o: ' . $count3;
+        echo '<br>';
+        echo 'Số ký tự d: ' . $count4;
+        echo '<br>';
+        echo 'Số ký tự w: ' . $count5;
     ?>
 </body>
 </html>
