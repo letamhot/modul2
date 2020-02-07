d683c4150e4236cd3d311666f06a2dc2f96b807c
LÊ VĂN TÁM <31695567+letamhot@users.noreply.github.com>
2020-02-07 14:42:18 +0700

    update

--- arr/count_obj/count_obj.php
+++ arr/count_obj/count_obj.php
@@ -3,70 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Count</title>
 </head>
 <body>
-<!-- <form action="count_obj.php" method="post">
-<input type="text" class="text" value="Nhập">
-<input type="submit" class="sub" value="Tổng">
-</form> -->
-    <?php 
-        $obj= "Hello World";
-        print_r($obj);
-        echo '<br>';
-        $count = null;
-        $count1 = null;
-        $count2 = null;
-        $count3 = null;
-        $count4 = null;
-        $count5 = null;
-        $position = null;
-        $position1 = null;
-        $position2 = null;
-        $position3 = null;
-        $position4 = null;
-        $position5 = null;
 
-        echo 'Tổng số kí tự: ' . strlen($obj) . '<br>';
-        for($i = 0; $i < strlen($obj); $i++){
-            if($obj[$i] == strtoupper('h') || $obj[$i] == strtolower('H')){
-                $position .= $i . '<br>';
-                $count++;
-            }
-            if($obj[$i] == strtoupper('e') || $obj[$i] == strtolower('E')){
-                $position1 .= $i . '<br>';
-                $count1++;
-            }
-            if($obj[$i] == strtoupper('l') || $obj[$i] == strtolower('L')){
-                $position2 .= $i . '<br>';
-                $count2++;
-            }
-            if($obj[$i] == strtoupper('o') || $obj[$i] == strtolower('O')){
-                $position3 .= $i . '<br>';
-                $count3++;
-            }
-            if($obj[$i] == strtoupper('d') || $obj[$i] == strtolower('D')){
-                $position4 .= $i . '<br>';
-                $count4++;
-            }
-            if($obj[$i] == strtoupper('w') || $obj[$i] == strtolower('W')){
-                $position5 .= $i . '<br>';
-                $count5++;
-            }
+<form action="count.php" method="post">
+    <input type="text" name="str" value="<?= $str??null ?>">
+    <input type="text" name="char" value="<?= $char??null ?>">
+    <input type="submit" name="sub" value="Tổng">
+</form>
+<?php
+if (isset($_POST['sub'])) {
+    $str = $_POST['str'];
+    echo 'Chuổi ký tự: ' .$str . '<br>';
+    $char = $_POST['char'];
+    echo 'Ký tự: ' . $char . '<br>';
+    $count = 0;
+    $possion = null;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($char === $str[$i]) {
+            $possion = $i;
+            $count++;
+            echo 'Vị trí xuất hiện: ' . $possion . '<br>';
         }
-        echo '<br>';
-        echo 'Số ký tự h: ' . $count;
-        echo '<br>';
-        echo 'Số ký tự e: ' . $count1;
-        echo '<br>';
-        echo 'Số ký tự l: ' . $count2;
-        echo '<br>';
-        echo 'Số ký tự o: ' . $count3;
-        echo '<br>';
-        echo 'Số ký tự d: ' . $count4;
-        echo '<br>';
-        echo 'Số ký tự w: ' . $count5;
-    ?>
+    }
+    echo 'Số lần xuất hiện của ký tự: ' . $count;
+}
+?>
 </body>
 </html>
